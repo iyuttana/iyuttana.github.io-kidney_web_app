685b9afac501d4c1d2e461f1dad6807d0ff908d8
iyuttana <real_fast94@hotmail.com>
2025-09-18 21:58:58 +0700

    Deploy Flutter Web to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2752074813"
+    serviceWorkerVersion: "2945414710"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b4dbb2a8c52181ce06b1570e9413bd60
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "3d28da02f69990e4e467220de63a3e31",
+"flutter_bootstrap.js": "ed6bea2a29259c9483d623c81312bcf5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
